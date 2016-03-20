0a9b5f1b7c352a9c86179e0f2fa5c91d2acfd324
Guy John <guy@rumblesan.com>
2016-03-20 19:16:26 +0000

    Move Vector type and functions into separate module

--- src/Main.hs
+++ src/Main.hs
@@ -11,42 +11,18 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+module Main where
+
+import Vector
+
 main :: IO()
 main = print $ Circle (normalize $ Vector 1 2 3 `cross` Vector 4 5 6) 3
 
 -- Basic Data Types
-data Vector = Vector Double Double Double deriving(Show)
 data Shape = Circle { position :: Vector, radius :: Double}
            | Plane { position :: Vector, normal :: Vector }
            | Triangle Vector Vector Vector
             deriving (Show)
-
--- Helper Functions
---
-add :: Vector -> Vector -> Vector
-(Vector x y z) `add` (Vector x1 y1 z1) = Vector (x + x1) (y + y1) (z + z1)
-
-sub :: Vector -> Vector -> Vector
-(Vector x y z) `sub` (Vector x1 y1 z1) = Vector (x - x1) (y - y1) (z - z1)
-
-scalarMult :: Vector -> Double -> Vector
-(Vector x y z) `scalarMult` s = Vector (x * s) (y * s) (z * s)
-
-dotProduct :: Vector -> Vector -> Double
-(Vector x y z) `dotProduct` (Vector x1 y1 z1) = x * x1 + y * y1 + z * z1
-
-cross :: Vector -> Vector -> Vector
-(Vector x y z) `cross` (Vector x1 y1 z1) = Vector (y * z1 + z *y1) (x * z1 + y * x1) (x * y1 + y * x1)
-
-normalize :: Vector -> Vector
-normalize (Vector x y z) = let m = magnitude (Vector x y z)
-                           in Vector (x / m) (y / m) (z / m)
-
-magnitude :: Vector -> Double
-magnitude (Vector x y z) = sqrt (x * x + y * y + z * z)
-
-neg :: Vector -> Vector
-neg (Vector x y z) = Vector (-x) (-y) (-z)
 
 -- Finds a,b, and c for a^2*x + b*x + c*x = 0, useful for finding intersections
 roots :: Double -> Double -> Double -> [Double]
