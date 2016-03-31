b82c73ea9d14c788f7b8c1b746586362e02a1bac
Guy John <guy@rumblesan.com>
2016-03-31 14:41:47 +0100

    Move camera structure and functions into module

---
+++ src/Camera.hs
@@ -0,0 +1,12 @@
+module Camera where
+
+import Vector
+
+data Camera = Camera { fov :: Double,
+                       cameraPosition :: Point,
+                       lookingAt :: Vector
+}
+
+eyeVector :: Camera -> Vector
+eyeVector cam = normalize $ lookingAt cam `sub` cameraPosition cam
+
