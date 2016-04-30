85a155bc098341ba6a512d48a79fe422a1545acf
Guy John <guy@rumblesan.com>
2016-04-30 11:38:47 +0100

    Add first tests for Shapes modules

--- test/Tests/Shapes.hs
+++ test/Tests/Shapes.hs
@@ -9,10 +9,14 @@ import Shapes
 import Ray
 import Vector
 
+import Data.Maybe (isJust)
+
 shapeTests :: Test
 shapeTests =
   testGroup "Shape Tests" [
-    testCase "Check sphere intersection works" test_sphereIntersection
+    testCase "Check sphere intersection works" test_sphereIntersection,
+    testCase "Check plane intersection works" test_planeIntersection,
+    testProperty "QuickCheck test for intersection" prop_rayIntersectonWorks
   ]
 
 test_sphereIntersection :: Assertion
@@ -24,4 +28,23 @@ test_sphereIntersection =
     distance = rayIntersection ray sphere
   in
     assertEqual "" distance expected
+
+test_planeIntersection :: Assertion
+test_planeIntersection =
+  let
+    plane = Plane (Vector 0 0 0) (Vector 0 1 0)
+    ray = Ray (Vector 0 10 0) (Vector 0 (-1) 0)
+    expected = Just 10 
+    distance = rayIntersection ray plane
+  in
+    assertEqual "" distance expected
+
+prop_rayIntersectonWorks :: (Double, Double) -> Bool
+prop_rayIntersectonWorks (xVal, zVal) =
+  let
+    plane = Plane (Vector 0 0 0) (Vector 0 1 0)
+    ray = Ray (Vector xVal 10 zVal) (Vector 0 (-1) 0)
+  in
+    isJust $ rayIntersection ray plane
+
 
